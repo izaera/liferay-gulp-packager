324c04b8bba10399e4ac3d91d88878e137ca3c46
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-04-06 15:11:54 +0200

    Give a proper name to transpiled modules so that they can be loaded.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var copyPackageJson = require('./lib/copy-package-json.js');
 var copyPackages = require('./lib/copy-packages.js');
 var transpileProject = require('./lib/transpile-project.js');
 var transpilePackages = require('./lib/transpile-packages.js');
+var nameTranspiledModules = require('./lib/name-transpiled-modules.js');
 
 function attach(gulp, options) {
 	options = options || {};
@@ -11,8 +12,9 @@ function attach(gulp, options) {
 	
 	gulp.task('lr:copyPackageJson', function(){return copyPackageJson(options)});
 	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
-	gulp.task('lr:transpileProject', function(){return transpileProject(options)});
+	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function(){return transpileProject(options)});
 	gulp.task('lr:transpilePackages', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return transpilePackages(options)});
+	gulp.task('lr:nameTranspiledModules', ['lr:transpileProject', 'lr:transpilePackages'], function(){return nameTranspiledModules(options)});
 
 	gulp.task(
 		'lr:all', 
@@ -20,7 +22,8 @@ function attach(gulp, options) {
 			'lr:copyPackageJson', 
 			'lr:copyPackages', 
 			'lr:transpileProject', 
-			'lr:transpilePackages'
+			'lr:transpilePackages',
+			'lr:nameTranspiledModules',
 		]
 	);
 
