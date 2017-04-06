622d73349bddfcb27f9cabca714e14b82cb567c2
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-04-06 10:25:56 +0200

    Add a task to transpile packages to AMD format

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var copyPackageJson = require('./lib/copy-package-json.js');
-var packageNpm = require('./lib/package-npm.js');
-var transpile = require('./lib/transpile.js');
+var copyPackages = require('./lib/copy-packages.js');
+var transpileProject = require('./lib/transpile-project.js');
+var transpilePackages = require('./lib/transpile-packages.js');
 
 function attach(gulp, options) {
 	options = options || {};
 	var task = options.task || 'default';
 	
-	gulp.task('lr:copy-package-json', function(){copyPackageJson(options)});
-	gulp.task('lr:package-npm', function(){packageNpm(options)});
-	gulp.task('lr:transpile', function(){transpile(options)});
+	gulp.task('lr:copyPackageJson', function(){return copyPackageJson(options)});
+	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
+	gulp.task('lr:transpileProject', function(){return transpileProject(options)});
+	gulp.task('lr:transpilePackages', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return transpilePackages(options)});
 
-	gulp.task('lr:all', ['lr:copy-package-json', 'lr:package-npm', 'lr:transpile']);
+	gulp.task(
+		'lr:all', 
+		[
+			'lr:copyPackageJson', 
+			'lr:copyPackages', 
+			'lr:transpileProject', 
+			'lr:transpilePackages'
+		]
+	);
 
 	gulp.task(task, ['lr:all']);
 }
