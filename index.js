ebc4d8e9c0e3f345153bf62562ec89996598047c
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-04-18 19:05:21 +0200

    Do not use Babel AMD rewriter because it fails for non one-line requires

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var copyPackageJson = require('./lib/copy-package-json.js');
 var copyPackages = require('./lib/copy-packages.js');
 var transpileProject = require('./lib/transpile-project.js');
-var transpilePackages = require('./lib/transpile-packages.js');
-var nameTranspiledModules = require('./lib/name-transpiled-modules.js');
+// var transpilePackages = require('./lib/transpile-packages.js');
+// var nameTranspiledModules = require('./lib/name-transpiled-modules.js');
+var wrapModules = require('./lib/wrap-modules.js');
 
 function attach(gulp, options) {
 	options = options || {};
@@ -13,8 +14,9 @@ function attach(gulp, options) {
 	gulp.task('lr:copyPackageJson', function(){return copyPackageJson(options)});
 	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
 	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function(){return transpileProject(options)});
-	gulp.task('lr:transpilePackages', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return transpilePackages(options)});
-	gulp.task('lr:nameTranspiledModules', ['lr:transpileProject', 'lr:transpilePackages'], function(){return nameTranspiledModules(options)});
+	// gulp.task('lr:transpilePackages', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return transpilePackages(options)});
+	// gulp.task('lr:nameTranspiledModules', ['lr:transpileProject', 'lr:transpilePackages'], function(){return nameTranspiledModules(options)});
+	gulp.task('lr:wrapModules', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return wrapModules(options)});
 
 	gulp.task(
 		'lr:all', 
@@ -22,8 +24,9 @@ function attach(gulp, options) {
 			'lr:copyPackageJson', 
 			'lr:copyPackages', 
 			'lr:transpileProject', 
-			'lr:transpilePackages',
-			'lr:nameTranspiledModules',
+			// 'lr:transpilePackages',
+			// 'lr:nameTranspiledModules',
+			'lr:wrapModules', 
 		]
 	);
 
