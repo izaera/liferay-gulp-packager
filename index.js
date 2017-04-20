afb47fdbea233845f0ae8b125670e4a0c1908cbc
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-04-20 10:20:42 +0200

    Avoid Babel AMD transpilation as it fails for many modules
    We will just simply use the local require in the loader to be able to wrap npm modules inside define calls without transpiling them.

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 'use strict';
 
 var copyPackageJson = require('./lib/copy-package-json.js');
-var copyPackages = require('./lib/copy-packages.js');
 var transpileProject = require('./lib/transpile-project.js');
-// var transpilePackages = require('./lib/transpile-packages.js');
-// var nameTranspiledModules = require('./lib/name-transpiled-modules.js');
-var wrapModules = require('./lib/wrap-modules.js');
+var nameProjectModules = require('./lib/name-project-modules.js');
+var copyPackages = require('./lib/copy-packages.js');
+var wrapPackageModules = require('./lib/wrap-package-modules.js');
 
 function attach(gulp, options) {
 	options = options || {};
 	var task = options.task || 'default';
 	
 	gulp.task('lr:copyPackageJson', function(){return copyPackageJson(options)});
-	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
 	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function(){return transpileProject(options)});
-	// gulp.task('lr:transpilePackages', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return transpilePackages(options)});
-	// gulp.task('lr:nameTranspiledModules', ['lr:transpileProject', 'lr:transpilePackages'], function(){return nameTranspiledModules(options)});
-	gulp.task('lr:wrapModules', ['lr:copyPackageJson', 'lr:copyPackages'], function(){return wrapModules(options)});
+	gulp.task('lr:nameProjectModules', ['lr:transpileProject'], function(){return nameProjectModules(options)});
+	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
+	gulp.task('lr:wrapPackageModules', ['lr:copyPackages'], function(){return wrapPackageModules(options)});
 
 	gulp.task(
 		'lr:all', 
 		[
 			'lr:copyPackageJson', 
-			'lr:copyPackages', 
 			'lr:transpileProject', 
-			// 'lr:transpilePackages',
-			// 'lr:nameTranspiledModules',
-			'lr:wrapModules', 
+			'lr:nameProjectModules', 
+			'lr:copyPackages', 
+			'lr:wrapPackageModules', 
 		]
 	);
 
