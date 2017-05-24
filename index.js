ff9544922e0dd8adb7810aa98f3a3a8450802f36
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-05-24 11:35:54 +0200

    Shimming with bundling dependencies inside JARs
    This is a dead end because it would require to detect shimming versions when copying packages which would lead to the need to rewrite package.json files adding the shims and so on. Also even if it could be implemented you could virtually have several versions of shimmed node.js’ running in the same page, which could lead to inconsistencies, given that node.js is versioned as a whole, not as a graph of packages.

--- index.js
+++ index.js
@@ -4,32 +4,49 @@ var copyPackageJson = require('./lib/copy-package-json.js');
 var transpileProject = require('./lib/transpile-project.js');
 var nameProjectModules = require('./lib/name-project-modules.js');
 var copyPackages = require('./lib/copy-packages.js');
+var shimServerModules = require('./lib/shim-server-modules.js');
+var shimGlobals = require('./lib/shim-globals.js');
 var wrapPackageModules = require('./lib/wrap-package-modules.js');
 
 function attach(gulp, options) {
 	options = options || {};
 	var task = options.task || 'default';
-	
-	gulp.task('lr:copyPackageJson', function(){return copyPackageJson(options)});
-	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function(){return transpileProject(options)});
-	gulp.task('lr:nameProjectModules', ['lr:transpileProject'], function(){return nameProjectModules(options)});
-	gulp.task('lr:copyPackages', function(){return copyPackages(options)});
-	gulp.task('lr:wrapPackageModules', ['lr:copyPackages'], function(){return wrapPackageModules(options)});
 
-	gulp.task(
-		'lr:all', 
-		[
-			'lr:copyPackageJson', 
-			'lr:transpileProject', 
-			'lr:nameProjectModules', 
-			'lr:copyPackages', 
-			'lr:wrapPackageModules', 
-		]
-	);
+	gulp.task('lr:copyPackageJson', function() {
+		return copyPackageJson(options);
+	});
+	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function() {
+		return transpileProject(options);
+	});
+	gulp.task('lr:nameProjectModules', ['lr:transpileProject'], function() {
+		return nameProjectModules(options);
+	});
+	gulp.task('lr:copyPackages', function() {
+		return copyPackages(options);
+	});
+	gulp.task('lr:shimServerModules', ['lr:copyPackages'], function() {
+		return shimServerModules(options);
+	});
+	gulp.task('lr:shimGlobals', ['lr:shimServerModules'], function() {
+		return shimGlobals(options);
+	});
+	gulp.task('lr:wrapPackageModules', ['lr:shimGlobals'], function() {
+		return wrapPackageModules(options);
+	});
+
+	gulp.task('lr:all', [
+		'lr:copyPackageJson',
+		'lr:transpileProject',
+		'lr:nameProjectModules',
+		'lr:copyPackages',
+		'lr:shimServerModules',
+		'lr:shimGlobals',
+		'lr:wrapPackageModules',
+	]);
 
 	gulp.task(task, ['lr:all']);
 }
 
 module.exports = {
-	attach: attach
-}
+	attach: attach,
+};
