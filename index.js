5fa39c761d78da790eea436aca15bb22ac16f6ae
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-06-13 17:07:17 -0700

    Support workflows

--- index.js
+++ index.js
@@ -1,71 +1,87 @@
 'use strict';
 
-var copyPackageJson = require('./lib/copy-package-json.js');
-var transpileProject = require('./lib/transpile-project.js');
-var nameProjectModules = require('./lib/name-project-modules.js');
-var copyPackages = require('./lib/copy-packages.js');
-var replaceBrowserMains = require('./lib/replace-browser-mains.js'); // iso
-var replaceBrowserModules = require('./lib/replace-browser-modules.js'); // iso
-var normalizeRequires = require('./lib/normalize-requires.js');
-var rewriteBrowserRequires = require('./lib/rewrite-browser-requires.js'); // iso
-var shimNodeGlobals = require('./lib/shim-node-globals.js'); // iso
-var shimNodeModules = require('./lib/shim-node-modules.js'); // iso
-var wrapPackageModules = require('./lib/wrap-package-modules.js');
-
 function attach(gulp, options) {
 	options = options || {};
 	var task = options.task || 'default';
+	var taskPrefix = options.taskPrefix || 'lr:';
+	var workflowName = options.workflow || 'isomorphic';
 
-	gulp.task('lr:copyPackageJson', function() {
-		return copyPackageJson(options);
-	});
-	gulp.task('lr:transpileProject', ['lr:copyPackageJson'], function() {
-		return transpileProject(options);
-	});
-	gulp.task('lr:nameProjectModules', ['lr:transpileProject'], function() {
-		return nameProjectModules(options);
-	});
-	gulp.task('lr:copyPackages', function() {
-		return copyPackages(options);
-	});
-	gulp.task('lr:replaceBrowserMains', ['lr:copyPackages'], function() {
-		return replaceBrowserMains(options);
-	});
-	gulp.task('lr:replaceBrowserModules', ['lr:replaceBrowserMains'], function() {
-		return replaceBrowserModules(options);
-	});
-	gulp.task('lr:normalizeRequires', ['lr:replaceBrowserModules'], function() {
-		return normalizeRequires(options);
-	});
-	gulp.task('lr:rewriteBrowserRequires', ['lr:normalizeRequires'], function() {
-		return rewriteBrowserRequires(options);
-	});
-	gulp.task('lr:shimNodeGlobals', ['lr:rewriteBrowserRequires'], function() {
-		return shimNodeGlobals(options);
-	});
-	gulp.task('lr:shimNodeModules', ['lr:shimNodeGlobals'], function() {
-		return shimNodeModules(options);
-	});
-	gulp.task('lr:wrapPackageModules', ['lr:shimNodeModules'], function() {
-		return wrapPackageModules(options);
-	});
+	// Define available tasks
+	var taskNames = {
+		copyPackageJson: 'copy-package-json',
+		copyPackages: 'copy-packages',
+		nameProjectModules: 'name-project-modules',
+		normalizeRequires: 'normalize-requires',
+		replaceBrowserMains: 'replace-browser-mains',
+		replaceBrowserModules: 'replace-browser-modules',
+		rewriteBrowserRequires: 'rewrite-browser-requires',
+		shimNodeGlobals: 'shim-node-globals',
+		shimNodeModules: 'shim-node-modules',
+		transpileProject: 'transpile-project',
+		wrapPackageModules: 'wrap-package-modules',
+	};
 
-	gulp.task('lr:all', [
-		'lr:copyPackageJson',
-		'lr:transpileProject',
-		'lr:nameProjectModules',
-		'lr:copyPackages',
-		'lr:replaceBrowserMains',
-		'lr:replaceBrowserModules',
-		'lr:normalizeRequires',
-		'lr:rewriteBrowserRequires',
-		'lr:shimNodeGlobals',
-		'lr:shimNodeModules',
-		'lr:wrapPackageModules',
-	]);
+	// Define workflows
+	var workflows = {
+		browser: {
+			// Project
+			copyPackageJson: [],
+			transpileProject: [],
+			nameProjectModules: ['copyPackageJson', 'transpileProject'],
+			// Packages
+			copyPackages: [],
+			replaceBrowserMains: ['copyPackages'],
+			replaceBrowserModules: ['replaceBrowserMains'],
+			normalizeRequires: ['replaceBrowserModules'],
+			rewriteBrowserRequires: ['normalizeRequires'],
+			wrapPackageModules: ['rewriteBrowserRequires'],
+		},
+		server: {
+			// Project
+			copyPackageJson: [],
+			transpileProject: [],
+			nameProjectModules: ['copyPackageJson', 'transpileProject'],
+			// Packages
+			copyPackages: [],
+			replaceBrowserMains: ['copyPackages'],
+			replaceBrowserModules: ['replaceBrowserMains'],
+			normalizeRequires: ['copyPackages'],
+			rewriteBrowserRequires: ['normalizeRequires'],
+			shimNodeGlobals: ['rewriteBrowserRequires'],
+			shimNodeModules: ['shimNodeGlobals'],
+			wrapPackageModules: ['shimNodeModules'],
+		},
+	};
 
-	if (task) {
-		gulp.task(task, ['lr:all']);
+	// Apply selected workflow
+	if (workflowName) {
+		var workflow = workflows[workflowName];
+		var taskIds = Object.keys(workflow);
+
+		taskIds.forEach(taskId => {
+			var taskName = taskNames[taskId];
+
+			gulp.task(
+				taskPrefix + taskName,
+				workflow[taskId].map(taskId => taskPrefix + taskNames[taskId]),
+				() => require('./lib/' + taskName + '.js')(options)
+			);
+		});
+
+		gulp.task(
+			taskPrefix + 'build',
+			taskIds.map(taskId => taskPrefix + taskNames[taskId])
+		);
+
+		if (task) {
+			gulp.task(task, [taskPrefix + 'build']);
+		}
+	} else {
+		taskIds.forEach(taskId => {
+			gulp.task(taskNames[taskId], [], () =>
+				require('./lib/' + taskNames[taskId] + '.js')(options)
+			);
+		});
 	}
 }
 
