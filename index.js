3bab016a31c02f5dec110281e8e63a271b074a3b
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-06-14 09:45:24 -0700

    Remove workflows (not needed any more)

--- index.js
+++ index.js
@@ -4,84 +4,42 @@ function attach(gulp, options) {
 	options = options || {};
 	var task = options.task || 'default';
 	var taskPrefix = options.taskPrefix || 'lr:';
-	var workflowName = options.workflow || 'isomorphic';
 
 	// Define available tasks
-	var taskNames = {
-		copyPackageJson: 'copy-package-json',
-		copyPackages: 'copy-packages',
-		nameProjectModules: 'name-project-modules',
-		normalizeRequires: 'normalize-requires',
-		replaceBrowserMains: 'replace-browser-mains',
-		replaceBrowserModules: 'replace-browser-modules',
-		rewriteBrowserRequires: 'rewrite-browser-requires',
-		shimNodeGlobals: 'shim-node-globals',
-		shimNodeModules: 'shim-node-modules',
-		transpileProject: 'transpile-project',
-		wrapPackageModules: 'wrap-package-modules',
+	var tasks = {
+		// Project
+		'copy-package-json': [],
+		'transpile-project': [],
+		'name-project-modules': ['copy-package-json', 'transpile-project'],
+		// Packages
+		'copy-packages': [],
+		'inject-package-dependencies': ['copy-packages'],
+		'replace-browser-mains': ['copy-packages'],
+		'replace-browser-modules': ['copy-packages'],
+		'normalize-requires': ['replace-browser-mains', 'replace-browser-modules'],
+		'rewrite-browser-requires': ['normalize-requires'],
+		'shim-node-globals': ['rewrite-browser-requires'],
+		'shim-node-modules': ['shim-node-globals'],
+		'wrap-package-modules': ['shim-node-modules'],
 	};
 
-	// Define workflows
-	var workflows = {
-		browser: {
-			// Project
-			copyPackageJson: [],
-			transpileProject: [],
-			nameProjectModules: ['copyPackageJson', 'transpileProject'],
-			// Packages
-			copyPackages: [],
-			replaceBrowserMains: ['copyPackages'],
-			replaceBrowserModules: ['replaceBrowserMains'],
-			normalizeRequires: ['replaceBrowserModules'],
-			rewriteBrowserRequires: ['normalizeRequires'],
-			wrapPackageModules: ['rewriteBrowserRequires'],
-		},
-		server: {
-			// Project
-			copyPackageJson: [],
-			transpileProject: [],
-			nameProjectModules: ['copyPackageJson', 'transpileProject'],
-			// Packages
-			copyPackages: [],
-			replaceBrowserMains: ['copyPackages'],
-			replaceBrowserModules: ['replaceBrowserMains'],
-			normalizeRequires: ['copyPackages'],
-			rewriteBrowserRequires: ['normalizeRequires'],
-			shimNodeGlobals: ['rewriteBrowserRequires'],
-			shimNodeModules: ['shimNodeGlobals'],
-			wrapPackageModules: ['shimNodeModules'],
-		},
-	};
-
-	// Apply selected workflow
-	if (workflowName) {
-		var workflow = workflows[workflowName];
-		var taskIds = Object.keys(workflow);
-
-		taskIds.forEach(taskId => {
-			var taskName = taskNames[taskId];
-
-			gulp.task(
-				taskPrefix + taskName,
-				workflow[taskId].map(taskId => taskPrefix + taskNames[taskId]),
-				() => require('./lib/' + taskName + '.js')(options)
-			);
-		});
+	Object.keys(tasks).forEach(name => {
+		var dependencies = tasks[name];
 
 		gulp.task(
-			taskPrefix + 'build',
-			taskIds.map(taskId => taskPrefix + taskNames[taskId])
+			taskPrefix + name,
+			dependencies.map(name => taskPrefix + name),
+			() => require('./lib/' + name + '.js')(options)
 		);
+	});
 
-		if (task) {
-			gulp.task(task, [taskPrefix + 'build']);
-		}
-	} else {
-		taskIds.forEach(taskId => {
-			gulp.task(taskNames[taskId], [], () =>
-				require('./lib/' + taskNames[taskId] + '.js')(options)
-			);
-		});
+	gulp.task(
+		taskPrefix + 'build',
+		Object.keys(tasks).map(name => taskPrefix + name)
+	);
+
+	if (task) {
+		gulp.task(task, [taskPrefix + 'build']);
 	}
 }
 
